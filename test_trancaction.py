9f9f60f303e5c1d12e24f3013499b3f762221820
Maksim <sakur0yakyra@gmail.com>
2026-02-03 21:05:42 +0300

    Add Transaction class and first test file

--- test_trancaction.py
+++ test_trancaction.py
@@ -2,10 +2,10 @@ from src.data.transaction import Transaction
 
 
 t = Transaction(1000, 2, 'RU')
-t2 = Transaction(500, 14, 'US')
+t2 = Transaction(500000, 24, 'US')
 print(t.to_dict())
-print('Night:', t.is_night())
+# print('Night:', t.is_night())
 
 
 print(t2.to_dict())
-print('Night:', t2.is_night())
+# print('Night:', t2.is_night())
